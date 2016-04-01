8732731b0e5bd5f167a016f4eb0aa34f3409337d
Thomas <info@von-gostomski.eu>
2016-04-01 21:37:40 +0200

    removed unused file, changed some stuff

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ bundle exec jekyll build
 cd ${HTML_FOLDER}
 
 # config
-git config --global user.email "nandomoreira.me@gmail.com"
-git config --global user.name "Fernando Moreira"
+git config --global user.email "info@von-gostomski.eu"
+git config --global user.name "Thomas von Gostomski"
 
 # deploy
 git init
